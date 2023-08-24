fd7bb2f33e102c31ce23972555e670a67593a64b
keithprimas <136135449+keithprimas@users.noreply.github.com>
2023-08-23 21:52:31 -0400

    changed background color and made events set to save to local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,47 +1,57 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.zz
 
 const currentDay = dayjs().format('MMMM D, YYYY');
-
 console.log(currentDay);
+
+const currentHour = dayjs().hour();
+console.log(currentHour);
 
 $(document).ready(function () {
 
-  function updateClass() {
-
-   let currentHour = dayjs().hour();
-
-  let elements = document.querySelectorAll("[id]");
-
-  elements.forEach(function(element) {
-
-    let hour = parseInt(element.id);
-
-    if (hour < currentHour) {
-      element.classList.add("past");
-    } else if (hour === currentHour) {
-      element.classList.add("present");
-    } else {
-      element.classList.add("future");
-    }
-  });
-}
-
-
-$("#currentDay").text(currentDay);
+  $("#currentDay").text(currentDay);
 
   $(".saveBtn").on("click", function() {
     let timeBlockId = $(this).parent().attr("id");
     let userInput = $(this).siblings(".description").val();
     localStorage.setItem(timeBlockId, userInput);
-
-    updateClass();
   });
 
- 
+  function eachTimeSlot() {
+      currentHour;
+      $(".time-block").each(function() {
+        let timeBlock = parseInt($(this).attr("id").split("hour"));
 
+        if (timeBlock < currentHour) {
+          $(this).addClass("past");
+          $(this).removeClass("present");
+          $(this).removeClass("future");
+        }
+
+        if (timeBlock === currentHour) {
+          $(this).removeClass("past");
+          $(this).addClass("present");
+          $(this).removeClass("future");
+        }
+
+        if (timeBlock > currentHour) {
+          $(this).removeClass("past");
+          $(this).removeClass("present");
+          $(this).addClass("future");
+        } 
+    })
+  }
+
+  $("#9 .description").val(localStorage.getItem("9"));
+  $("#10 .description").val(localStorage.getItem("10"));
+  $("#11 .description").val(localStorage.getItem("11"));
+  $("#12 .description").val(localStorage.getItem("12"));
+  $("#13 .description").val(localStorage.getItem("13"));
+  $("#14 .description").val(localStorage.getItem("14"));
+  $("#15 .description").val(localStorage.getItem("15"));
+  $("#16 .description").val(localStorage.getItem("16"));
+  $("#17 .description").val(localStorage.getItem("17"));
  
+ eachTimeSlot();
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
